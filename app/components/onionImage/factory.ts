46d3718f75025a6f924bb2872d9b03dcb59fd834
singuerinc <nahuel.scotti@gmail.com>
2018-10-07 23:20:22 +0200

    style: remove commented code

--- app/components/onionImage/factory.ts
+++ app/components/onionImage/factory.ts
@@ -14,32 +14,3 @@ export const factory = (id: string = uuid(), props = {}): IOnionImage => ({
   locked: false,
   ...props
 });
-
-// interface State {
-//   onions: IOnionImage[];
-// }
-
-// export const addOnionImage = (paths: string[]) => ({ onions }: State) => {
-//   if (paths) {
-//     const images = R.map((path) => {
-//       return {
-//         ...factory(uuid()),
-//         src: path
-//       };
-//     }, paths);
-
-//     return { onions: [...onions, ...images] };
-//   }
-
-//   return { onions };
-// };
-
-// const hasSameId = R.curry((id: string, x: IOnionImage) => x.id === id);
-
-// export const removeOnionImage = (id: string) => ({ onions }: State) => {
-//   const filtered: IOnionImage[] = R.reject(hasSameId(id), onions);
-
-//   return {
-//     onions: filtered
-//   };
-// };
