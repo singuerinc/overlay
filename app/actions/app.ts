f8cd70c6d5e672c1e3540c7df4ad45371a342279
singuerinc <nahuel.scotti@gmail.com>
2018-10-07 22:56:00 +0200

    feat: create/remove guides with redux

--- app/actions/app.ts
+++ app/actions/app.ts
@@ -1,6 +1,14 @@
-export const SET_ACTIVE_GUIDE = 'SET_ACTIVE_GUIDE';
-export function setActiveGuide() {
-  return {
-    type: SET_ACTIVE_GUIDE
-  };
-}
+import { IGuide } from '../components/guide/IGuide';
+
+export const ADD_GUIDE = 'ADD_GUIDE';
+export const REMOVE_GUIDE = 'REMOVE_GUIDE';
+
+export const addGuide = (guide: IGuide) => ({
+  type: ADD_GUIDE,
+  payload: { guide }
+});
+
+export const removeGuide = (guide: IGuide) => ({
+  type: REMOVE_GUIDE,
+  payload: { guide }
+});
--- app/reducers/app.ts
+++
@@ -1,9 +0,0 @@
-import { SET_ACTIVE_GUIDE } from '../actions/app';
-export default function app(state = 0, action) {
-  switch (action.type) {
-    case SET_ACTIVE_GUIDE:
-      return state + 1;
-    default:
-      return state;
-  }
-}
