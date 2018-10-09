f0baee4015c4554cbf6f3cb362ba9cd0ed8c1786
singuerinc <nahuel.scotti@gmail.com>
2018-10-09 15:09:45 +0200

    feat: save allowAnalytics value in the storage

--- app/components/core/analytics.ts
+++ app/components/core/analytics.ts
@@ -7,10 +7,10 @@ const appVersion = '0.5.0';
 
 const firstRun = store.get('firstRun') || true;
 const userId = store.get('userId') || uuid();
+const allowAnalytics = (): boolean => store.get('allowAnalytics') || true;
 
 store.set('firstRun', false);
 store.set('userId', userId);
-store.set('optOut', false);
 
 const analytics = new Analytics(trackingCode, {
   appName: 'Overlay',
@@ -24,8 +24,10 @@ export const track = (
   label: string,
   value: number = 1
 ) => {
-  console.log('Tracking', `v${appVersion}`, category, action, label, value);
-  analytics.send('event', { ec: category, ea: action, el: label, ev: value });
+  if (allowAnalytics()) {
+    // console.log('Tracking', `v${appVersion}`, category, action, label, value);
+    analytics.send('event', { ec: category, ea: action, el: label, ev: value });
+  }
 };
 
 export const initializeAnalytics = () => {
