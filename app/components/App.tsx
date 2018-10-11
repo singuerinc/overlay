0abaaa74ad0687596d25b2e850d31c452f935727
singuerinc <nahuel.scotti@gmail.com>
2018-10-11 23:42:43 +0200

    refactor: App functional comp

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { removeGuide } from '../actions/guides';
-import { removeOnion } from '../actions/onions';
 import { Column } from '../components/column/Column';
 import { Grid } from '../components/grid/Grid';
 import { Guide } from '../components/guide/Guide';
@@ -32,63 +30,54 @@ interface IProps {
   tools: IToolsStore;
 }
 
-class AppView extends React.Component<IProps> {
-  public render() {
-    const {
-      columns,
-      grids,
-      guides,
-      onions,
-      rulers,
-      settings,
-      tools,
-      help
-    } = this.props;
-    const isToolsVisible = tools.visible;
-    const isHelpVisible = help.visible;
-    const isSettingsVisible = settings.visible;
+const AppView = ({
+  columns,
+  grids,
+  guides,
+  onions,
+  rulers,
+  settings,
+  tools,
+  help
+}: IProps) => {
+  const isToolsVisible = tools.visible;
+  const isHelpVisible = help.visible;
+  const isSettingsVisible = settings.visible;
 
-    const shouldDisplayTools =
-      !isHelpVisible && !isSettingsVisible && isToolsVisible;
+  const shouldDisplayTools =
+    !isHelpVisible && !isSettingsVisible && isToolsVisible;
 
-    return (
-      <>
-        <ToolsWrapper visible={shouldDisplayTools}>
-          {grids.map((grid: IGrid) => (
-            <Grid key={grid.id} {...grid} />
-          ))}
-          {columns.map((column: IColumn) => (
-            <Column key={column.id} {...column} />
-          ))}
-          {rulers.map((ruler: IRuler) => (
-            <Ruler key={ruler.id} {...ruler} />
-          ))}
-          {onions.map((onion: IOnionImage) => (
-            <OnionImage key={onion.id} {...onion} />
-          ))}
-          {guides.map((guide: IGuide) => (
-            <Guide key={guide.id} {...guide} />
-          ))}
-        </ToolsWrapper>
+  return (
+    <>
+      <ToolsWrapper visible={shouldDisplayTools}>
+        {grids.map((grid: IGrid) => (
+          <Grid key={grid.id} {...grid} />
+        ))}
+        {columns.map((column: IColumn) => (
+          <Column key={column.id} {...column} />
+        ))}
+        {rulers.map((ruler: IRuler) => (
+          <Ruler key={ruler.id} {...ruler} />
+        ))}
+        {onions.map((onion: IOnionImage) => (
+          <OnionImage key={onion.id} {...onion} />
+        ))}
+        {guides.map((guide: IGuide) => (
+          <Guide key={guide.id} {...guide} />
+        ))}
+      </ToolsWrapper>
 
-        {isHelpVisible && <Help />}
-        {isSettingsVisible && <Settings />}
-        <Toolbox x={10} y={10} />
-      </>
-    );
-  }
-}
+      {isHelpVisible && <Help />}
+      {isSettingsVisible && <Settings />}
+      <Toolbox x={10} y={10} />
+    </>
+  );
+};
 
-const App = connect(
-  (store: IAppStore, ownProps) => store,
-  {
-    removeGuide,
-    removeOnion
-  }
-)(AppView);
-
-export { App };
-
-const ToolsWrapper = styled.div<{ visible }>`
+const ToolsWrapper = styled.div<{ visible: boolean }>`
   display: ${({ visible }) => (visible ? 'block' : 'none')};
 `;
+
+const App = connect((store: IAppStore) => store)(AppView);
+
+export { App };
