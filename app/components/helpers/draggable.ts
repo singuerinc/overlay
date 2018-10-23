e4142d842474b57b06f05abf7c1563885f225fdf
singuerinc <nahuel.scotti@gmail.com>
2018-10-23 22:47:30 +0200

    fix: rounds coords on toolbox

--- app/components/helpers/draggable.ts
+++ app/components/helpers/draggable.ts
@@ -8,6 +8,14 @@ export const draggable = (
   const { innerWidth, innerHeight } = window;
 
   interactjs(el).draggable({
+    onend: ({ target }) => {
+      const x = parseInt(target.getAttribute('data-x'), 10);
+      const y = parseInt(target.getAttribute('data-y'), 10);
+
+      setPositionInDOM(el, x, y);
+
+      callback({ x, y });
+    },
     onmove: ({ dx, dy, target }) => {
       let x = (parseFloat(target.getAttribute('data-x')) || 0) + dx;
       let y = (parseFloat(target.getAttribute('data-y')) || 0) + dy;
