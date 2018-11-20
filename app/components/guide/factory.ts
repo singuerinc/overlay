3747278732262e87d14bd96d2322e70ec238d2c2
singuerinc <nahuel.scotti@gmail.com>
2018-11-20 12:03:45 +0100

    feat: default positions

--- app/components/guide/factory.ts
+++ app/components/guide/factory.ts
@@ -3,12 +3,23 @@ import { Color } from '../../utils/Color';
 import { GuideOrientation } from './GuideOrientation';
 import { IGuide } from './IGuide';
 
-export const factory = (id: string = uuid(), props = {}): IGuide => ({
-  color: Color.RED,
-  id,
-  locked: false,
+export const DEFAULT_GUIDE_LOCKED: boolean = false;
+export const DEFAULT_GUIDE_COLOR: Color = Color.RED;
+export const DEFAULT_GUIDE_X: number = 0;
+export const DEFAULT_GUIDE_Y: number = 0;
+export const DEFAULT_GUIDE_ORIENTATION: GuideOrientation =
+  GuideOrientation.HORIZONTAL;
+
+const defaults = {
+  color: DEFAULT_GUIDE_COLOR,
+  locked: DEFAULT_GUIDE_LOCKED,
   orientation: GuideOrientation.HORIZONTAL,
-  x: 0,
-  y: 100,
+  x: DEFAULT_GUIDE_X,
+  y: DEFAULT_GUIDE_Y
+};
+
+export const factory = (props?): IGuide => ({
+  id: uuid(),
+  ...defaults,
   ...props
 });
--- app/components/ruler/factory.ts
+++ app/components/ruler/factory.ts
@@ -2,15 +2,27 @@ import * as uuid from 'uuid/v1';
 import { Color } from '../../utils/Color';
 import { IRuler } from './IRuler';
 
-const factory = (id: string = uuid(), props = {}): IRuler => ({
-  color: Color.RED,
-  height: 400,
-  id,
-  locked: false,
-  opacity: 0.5,
-  width: 400,
-  x: 250,
-  y: 250,
+export const DEFAULT_RULER_WIDTH: number = 400;
+export const DEFAULT_RULER_HEIGHT: number = 400;
+export const DEFAULT_RULER_OPACITY: number = 0.5;
+export const DEFAULT_RULER_LOCKED: boolean = false;
+export const DEFAULT_RULER_COLOR: Color = Color.RED;
+export const DEFAULT_RULER_X: number = 0;
+export const DEFAULT_RULER_Y: number = 0;
+
+const defaults = {
+  color: DEFAULT_RULER_COLOR,
+  height: DEFAULT_RULER_HEIGHT,
+  locked: DEFAULT_RULER_LOCKED,
+  opacity: DEFAULT_RULER_OPACITY,
+  width: DEFAULT_RULER_WIDTH,
+  x: DEFAULT_RULER_X,
+  y: DEFAULT_RULER_Y
+};
+
+const factory = (props?): IRuler => ({
+  id: uuid(),
+  ...defaults,
   ...props
 });
 
