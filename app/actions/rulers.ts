14671aa05aadedfb70a51a656f6ea21625d77b7b
singuerinc <nahuel.scotti@gmail.com>
2018-11-20 15:02:54 +0100

    feat: split ruler

--- app/actions/rulers.ts
+++ app/actions/rulers.ts
@@ -1,7 +1,10 @@
+import { SplitDirection } from '../components/ruler/SplitDirection';
+
 export const ADD_RULER = 'ADD_RULER';
 export const REMOVE_RULER = 'REMOVE_RULER';
 export const RULER_SET_LOCK = 'RULER_SET_LOCK';
 export const RULER_SET_COLOR = 'RULER_SET_COLOR';
+export const RULER_SPLIT = 'RULER_SPLIT';
 
 export const add = () => ({
   type: ADD_RULER
@@ -20,4 +23,16 @@ export const setLock = (id: string, locked: boolean) => ({
 export const setColor = (id: string, color: string) => ({
   payload: { id, color },
   type: RULER_SET_COLOR
+});
+
+export const split = (
+  splitDirection: SplitDirection,
+  id: string,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) => ({
+  payload: { id, splitDirection, x, y, width, height },
+  type: RULER_SPLIT
 });
