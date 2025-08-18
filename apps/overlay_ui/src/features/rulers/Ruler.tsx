7e36fa6ceeb6c0b1789905c05d9934b95a190011
Nahuel Scotti <nahuel.scotti@gmail.com>
2025-08-18 22:27:59 +0200

    feat(ruler): ruler

--- apps/overlay_ui/src/features/rulers/Ruler.tsx
+++ apps/overlay_ui/src/features/rulers/Ruler.tsx
@@ -10,9 +10,10 @@ import { useCallback } from "react";
 
 const variantsInfo = cva(
   [
+    "absolute",
     "whitespace-nowrap text-xs tabular-nums",
     "select-none w-fit",
-    "h-8 flex flex-col",
+    "h-6 text-[10px] flex",
   ],
   {
     variants: {},
@@ -34,6 +35,11 @@ const variantsOriginGuideline = cva(["absolute border-neutral-200"], {
 export function Ruler() {
   const { data: ruler } = useGetRulerQuery();
   const setOriginCommand = useSetOriginRulerCommand();
+
+  const handleResetOrigin = useCallback(() => {
+    setOriginCommand.execute(0, 0);
+  }, [setOriginCommand]);
+
   const handleClick = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
       if (!event.altKey) {
@@ -61,8 +67,12 @@ export function Ruler() {
       }}
       onClick={handleClick}
     >
-      <HorizontalRuler origin={ruler.originX} />
+      <div
+        className="absolute top-0 left-0 w-6 h-6 bg-neutral-100 z-10 cursor-pointer"
+        onClick={handleResetOrigin}
+      />
       <VerticalRuler origin={ruler.originY} />
+      <HorizontalRuler origin={ruler.originX} />
       <NormalizedPositionX />
       <NormalizedPositionY />
       <StaticHorizontalGuideline originX={ruler.originX} />
@@ -97,32 +107,36 @@ function HorizontalRuler({ origin }: { origin: number }) {
   // FIXME: the number of items should be calculated based on the width/height of the ruler
   const numList = Array.from({ length: 40 }, (_, i) => i);
   return (
-    <div className="absolute top-0 left-0 h-8 w-full flex">
+    <div className="absolute top-0 left-0 h-6 w-full flex text-neutral-400 text-[10px] select-none">
       <div
-        className="h-full bg-neutral-200/50 shrink-0 grow-0"
+        className="h-full bg-neutral-100/50 shrink-0 grow-0"
         style={{ width: `${origin}px` }}
       >
-        <ol className="flex flex-row-reverse w-full h-full items-end text-neutral-300 text-xs select-none">
+        <ol className="flex flex-row-reverse w-full h-full items-end">
           {numList.map((num) => (
             <li
               key={num}
-              className="w-[50px] flex flex-col items-start gap-y-1 shrink-0"
+              className="w-[50px] flex flex-col items-start gap-y-px shrink-0"
             >
-              <span className="-translate-x-1/2">{(num + 1) * 50}</span>
-              <span className="border-l w-px h-2" />
+              <span className="-translate-x-1/2 text-neutral-300 h-[18px] flex flex-col justify-center">
+                {-(num + 1) * 50}
+              </span>
+              <span className="border-l border-neutral-300 w-px h-1.5" />
             </li>
           ))}
         </ol>
       </div>
       <div className="h-full w-full grow bg-neutral-100/50">
-        <ol className="flex w-full h-full items-end text-neutral-300 text-xs select-none">
+        <ol className="flex w-full h-full items-end">
           {numList.map((num) => (
             <li
               key={num}
-              className="w-[50px] flex flex-col items-start gap-y-1 shrink-0"
+              className="w-[50px] flex flex-col items-start gap-px shrink-0"
             >
-              <span className="-translate-x-1/2">{num * 50}</span>
-              <span className="border-l w-px h-2" />
+              <span className="-translate-x-1/2 h-[18px] flex flex-col justify-center">
+                {num * 50}
+              </span>
+              <span className="border-l w-px h-1.5" />
             </li>
           ))}
         </ol>
@@ -132,36 +146,44 @@ function HorizontalRuler({ origin }: { origin: number }) {
 }
 
 function VerticalRuler({ origin }: { origin: number }) {
-  const numList = Array.from({ length: 10 }, (_, i) => i);
+  // FIXME: the number of items should be calculated based on the width/height of the ruler
+  const numList = Array.from({ length: 40 }, (_, i) => i);
   return (
-    <div className="-rotate-90 origin-top-left rotate-45 top-0 left-0 absolute w-full">
-      <div className="relative top-0 left-0 h-8 w-full flex">
-        <div className="h-full w-full bg-neutral-100/50">
-          <ol className="flex flex-row-reverse w-full h-full items-end text-neutral-300 text-xs select-none">
+    <div className="absolute w-6 top-0 h-full left-0 text-neutral-400 text-[10px]">
+      <div className="relative top-0 left-0 h-full w-full flex flex-col">
+        <div
+          className="w-full bg-neutral-100/50 shrink-0 grow-0"
+          style={{ height: `${origin}px` }}
+        >
+          <ol className="flex flex-col-reverse w-full h-full items-end select-none">
             {numList.map((num) => (
               <li
                 key={num}
-                className="w-[50px] flex flex-col items-start gap-y-1 shrink-0"
+                className="h-[50px] w-full flex items-end gap-x-px shrink-0"
               >
-                <span className="-translate-x-1/2">{num * 50}</span>
-                <span className="border-l w-px h-2" />
+                <div className="w-full relative">
+                  <span className="-rotate-90 origin-top-left text-neutral-300 translate-y-[25px] h-4 absolute w-[50px] text-center">
+                    {-num * 50}
+                  </span>
+                </div>
+                <span className="border-b border-neutral-300 h-px w-2 shrink-0 grow-0" />
               </li>
             ))}
           </ol>
         </div>
-        <div
-          className="h-full bg-red-500/50 shrink-0 grow-0"
-          style={{ width: `${origin}px` }}
-        >
-          <ol className="flex w-full h-full items-end text-neutral-300 text-xs select-none">
-            <li className="w-[50px] shrink-0"></li>
+        <div className="w-full h-full bg-neutral-100/50 grow">
+          <ol className="flex flex-col w-full h-full items-end select-none">
             {numList.map((num) => (
               <li
                 key={num}
-                className="w-[50px] flex flex-col items-start gap-y-1 shrink-0"
+                className="h-[50px] w-full flex items-end gap-x-px shrink-0"
               >
-                <span className="-translate-x-1/2">{(num + 1) * 50}</span>
-                <span className="border-l w-px h-2" />
+                <div className="w-full relative">
+                  <span className="-rotate-90 origin-top-left translate-y-[25px] h-4 absolute w-[50px] text-center">
+                    {(num + 1) * 50}
+                  </span>
+                </div>
+                <span className="border-b h-px w-2 shrink-0 grow-0" />
               </li>
             ))}
           </ol>
@@ -186,8 +208,8 @@ function NormalizedPositionX() {
       style={{ transform: `translate(${x}px` }}
     >
       <span className="grow flex items-center pl-1">{normalizedX}</span>
-      <div className="h-2" />
-      {/* <div className="absolute bg-gradient-to-r from-10% from-transparent via-white to-90% to-transparent -z-10 -translate-x-14 h-6 left-0 w-32"></div> */}
+      <div className="h-1.5" />
+      <div className="absolute bg-gradient-to-r from-10% from-transparent via-neutral-100 to-90% to-transparent -z-10 -translate-x-14 h-6 left-0 w-32"></div>
     </div>
   );
 }
@@ -206,6 +228,7 @@ function NormalizedPositionY() {
       className={cn(variantsInfo(), "top-2")}
       style={{
         transform: `translateY(${y}px) rotate(-90deg) translateX(-100%)`,
+        // transform: `translateY(${y}px) rotate(-90deg)`,
         transformOrigin: "top left",
       }}
     >
